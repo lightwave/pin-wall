f76326775f43b16db1460faf03b90287b9ed893b
Chris Wong <chris@chriswongstudio.com>
2018-08-05 14:22:40 -0400

    For displaying user's pins, use Immutable.js

--- store.js
+++ store.js
@@ -3,10 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import { createActions, handleActions } from 'redux-actions';
 import promiseMiddleware from 'redux-promise';
+import { List, Map } from 'immutable';
 
 const defaultInitialState = {
   userWallInfos: [],
-  currentPins: [],
+  currentPins: List(),
 };
 
 // REDUCERS
@@ -19,7 +20,12 @@ export const reducer = handleActions(
 
     GET_USER_PINS: (state, action) => ({
       ...state,
-      currentPins: action.payload,
+      currentPins: List(action.payload),
+    }),
+
+    ADD_LINK: (state, action) => ({
+      ...state,
+      currentPins: [action.payload].concat(state.currentPins), // TODO: Not efficient but that'd do for now.
     }),
 
     RESET: (state, action) => ({...defaultInitialState}),
